f4e51efcedf830af76ad4edb2af2d4db878c8bf7
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-21 14:28:23 -0500

    will consider adding a game mode later.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,15 @@
-"use client";
-import { useGameStore } from "@/components/store/useGameStore";
 import dynamic from "next/dynamic";
-
+import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
 const Chat = dynamic(() => import("@/components/Chat"), {
 	ssr: false,
 });
 
 // Client component
-export const Nav = ({ accessToken }: { accessToken: string }) => {
-	const { configId, setConfigId } = useGameStore();
-
+export const Nav = async () => {
+	const accessToken = await getHumeAccessToken();
+	if (!accessToken) {
+		throw new Error("No access token");
+	}
 	return (
 		<div className="px-4 py-2 h-14 z-50 bg-card border-b border-border">
 			<Chat accessToken={accessToken} />
